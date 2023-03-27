aa115c0281db3a394080b0b140b73c7bfe3414ac
designdefined.by@gmail.com <designdefined.by@gmail.com>
2023-03-27 14:51:14 +0900

    basic router settings

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,32 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        homeLayout <Outlet />
+      </div>
+    ),
+    errorElement: <div>errorLayout</div>,
+    children: [
+      {
+        path: "home",
+        element: <div>home</div>,
+        index: true,
+      },
+      { path: "new", element: <div>new</div> },
+    ],
+  },
+  { path: "login", element: <div>login</div> },
+  { path: "register", element: <div>register</div> },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
