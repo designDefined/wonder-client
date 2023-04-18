21b5c9f32f23d4e051ec7afbb36d0b062f6fe753
designdefined.by@gmail.com <designdefined.by@gmail.com>
2023-04-18 20:37:34 +0900

    server connect test

--- src/entity/user/user.ts
+++ src/entity/user/user.ts
@@ -1,11 +1,18 @@
+export type UserPlatformType = "ADMIN" | "NAVER";
 export type UserId = number;
 export type UserName = string;
 export type UserNickname = string | null;
-export type UserPhoneNumber = string;
+export type UserPhoneNumber = string | null;
+export type UserEmail = string;
+export type UserDate = Date;
 
 export type User = {
+  platformType: UserPlatformType;
   id: UserId;
   name: UserName;
   nickname: UserNickname;
   phoneNumber: UserPhoneNumber;
+  email: UserEmail;
+  created_at: UserDate;
+  last_modified_at: UserDate;
 };
--- src/pages/NoLayout/Dev/Dev.tsx
+++ src/pages/NoLayout/Dev/Dev.tsx
@@ -13,6 +13,22 @@ export default function Dev() {
         onChange={(e) => setText(e.target.value)}
         placeHolder="플레이스홀더"
       />
+      <button
+        onClick={async () => {
+          try {
+            const response = await fetch("http://localhost:8000/echo", {
+              method: "POST",
+              mode: "cors",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify({ data: "sample" }),
+            });
+            const parsed = await response.json();
+            console.log(parsed);
+          } catch (e) {}
+        }}
+      >
+        핑
+      </button>
     </div>
   );
 }
