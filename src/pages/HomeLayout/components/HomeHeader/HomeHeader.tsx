ceffb93295c69a1b536b16b157b3c02c966817e8
designdefined.by@gmail.com <designdefined.by@gmail.com>
2023-04-20 11:38:53 +0900

    add Home Header

--- src/pages/HomeLayout/components/HomeHeader/HomeHeader.tsx
+++ src/pages/HomeLayout/components/HomeHeader/HomeHeader.tsx
@@ -1,11 +1,26 @@
 import styles from "./HomeHeader.module.scss";
+import shuffleIcon from "../assets/shuffle.svg";
+import searchIcon from "../assets/search.svg";
+
 import HeaderLogo from "../../../../components/logos/HeaderLogo/HeaderLogo";
+import { useMyAccountStore } from "../../../../store/account/useMyAccountStore";
+import classNames from "classnames/bind";
+import SearchBar from "../SearchBar/SearchBar";
+
+const cx = classNames.bind(styles);
 
 export default function HomeHeader() {
+  const isLoggedIn = useMyAccountStore((state) => state.isLoaded);
   return (
     <header className={styles.HomeHeader}>
       <HeaderLogo />
-      <div className={styles.tools}>로그인</div>
+      <div className={styles.tools}>
+        <SearchBar />
+        <button className={styles.shuffle}>
+          <img src={shuffleIcon} />
+        </button>
+        <button className={styles.login}>로그인</button>
+      </div>
     </header>
   );
 }
---
+++ src/pages/HomeLayout/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,30 @@
+import styles from "./SearchBar.module.scss";
+import searchIcon from "../assets/search.svg";
+import classNames from "classnames/bind";
+import { useState } from "react";
+
+const cx = classNames.bind(styles);
+
+export default function SearchBar() {
+  const [isSearching, setIsSearching] = useState(false);
+  const [searchInput, setSearchInput] = useState("");
+  return (
+    <div className={cx("SearchBarWrapper", { isSearching })}>
+      <button
+        className={styles.searchButton}
+        onClick={() => {
+          if (!isSearching) setIsSearching(true);
+        }}
+      >
+        <img src={searchIcon} />
+      </button>
+      {isSearching && (
+        <input
+          className={cx("searchBar")}
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+      )}
+    </div>
+  );
+}
