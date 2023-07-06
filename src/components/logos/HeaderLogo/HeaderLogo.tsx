2e34bf1399e5b3eb2099da070bc71b76fab8e02d
designDefined <designdefined.by@gmail.com>
2023-07-06 18:54:52 +0900

    Feat: logo animaition

--- src/components/logos/HeaderLogo/HeaderLogo.tsx
+++ src/components/logos/HeaderLogo/HeaderLogo.tsx
@@ -3,7 +3,10 @@ import logoMini from "../assets/logo_mini.svg";
 import { navigate } from "../../../libs/Codex";
 export default function HeaderLogo() {
   return (
-    <a className={styles.HeaderLogo} onClick={() => navigate("/")}>
+    <a
+      className={styles.HeaderLogo}
+      onClick={() => navigate("/", "slidePrevious")}
+    >
       <img src={logoMini} />
     </a>
   );
