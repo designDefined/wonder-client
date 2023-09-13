52841d8014ffc2e49a97440d22c77a5b09f6e474
designDefined <designdefined.by@gmail.com>
2023-09-14 03:04:56 +0900

    feat: Add title component

---
+++ src/components/Title/Title.tsx
@@ -0,0 +1,57 @@
+import classNames from "classnames/bind";
+import { ReactNode } from "react";
+import { navigate } from "../../libs/Codex";
+import styles from "./Title.module.scss";
+
+const cx = classNames.bind(styles);
+
+type TitleOnlyProps = {
+  title?: string;
+  className?: string;
+  children?: never;
+};
+
+type TitleProps = TitleOnlyProps & {
+  onClick: (() => void) | string;
+};
+
+function Title({ title, className }: TitleOnlyProps) {
+  return (
+    <h1 className={cx("Title", "label", "withoutMore", className)}>{title}</h1>
+  );
+}
+
+function More({
+  title,
+  className,
+  onClick,
+  moreText,
+}: TitleProps & { moreText?: string }) {
+  return (
+    <div className={cx("Title", className)}>
+      <h1 className={cx("label")}>{title}</h1>
+      {typeof onClick === "function" ? (
+        <button className={cx("more")}>{moreText ?? "더보기"}</button>
+      ) : (
+        <a
+          className={cx("more")}
+          onClick={(e) => {
+            e.preventDefault();
+            navigate(onClick);
+          }}
+        >
+          {moreText ?? "더보기"}
+        </a>
+      )}
+    </div>
+  );
+}
+
+function Arrow({}) {
+  return <div></div>;
+}
+
+Title.More = More;
+Title.Arrow = Arrow;
+
+export default Title;
--- src/pages/Dev/Dev.tsx
+++ src/pages/Dev/Dev.tsx
@@ -1,18 +1,32 @@
+import classNames from "classnames/bind";
 import { Button } from "../../components/Button/Button";
+import Title from "../../components/Title/Title";
+import styles from "./Dev.module.scss";
+
+const cx = classNames.bind(styles);
 
 export default function Dev() {
   return (
-    <main>
-      <Button isFullWidth isMainColored>
-        주 버튼
-      </Button>
-      <Button isFullWidth>부 버튼</Button>
-      <Button isMainColored>주 버튼</Button>
-      <Button>부 버튼</Button>
-      <Button isSmallSized isMainColored>
-        주 버튼
-      </Button>
-      <Button isSmallSized>부 버튼</Button>
+    <main className={cx("Dev")}>
+      <section className={cx("section")}>
+        <Title title="Buttons" />
+        <Button isFullWidth isMainColored>
+          주 버튼
+        </Button>
+        <Button isFullWidth>부 버튼</Button>
+        <Button isMainColored>주 버튼</Button>
+        <Button>부 버튼</Button>
+        <Button isSmallSized isMainColored>
+          주 버튼
+        </Button>
+        <Button isSmallSized>부 버튼</Button>
+      </section>
+      <section className={cx("section")}>
+        <Title title="Titles" />
+        <Title title="Normal Title" />
+        <Title.More title="More Title" onClick="/dev" />
+        <Title.More title="Arrow Title" onClick="/dev" />
+      </section>
     </main>
   );
 }
