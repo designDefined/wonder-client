e057e45a07566ac24adaf6793b0ca003c18f6ab1
designDefined <designdefined.by@gmail.com>
2023-10-15 02:14:07 +0900

    feat: Design register page

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,6 +1,8 @@
-import Button from "../../components/common/Button/Button";
-import TextInput from "../../components/common/TextInput/TextInput";
+import classNames from "classnames/bind";
+import Button from "../../components/Button/Button";
 import DefaultHeader from "../../components/headers/DefaultHeader/DefaultHeader";
+import Input from "../../components/Input/Input";
+import Progress from "../../components/Progress/Progress";
 import useEnhancedState from "../../libs/ReactAssistant/useEnhancedState";
 import {
   isValidRegisterEmail,
@@ -10,6 +12,9 @@ import {
 import { registerUser, useAccount } from "../../store/account/useAccount";
 import { UserRegisterForm } from "../../types/user/userAuthorization";
 import styles from "./Register.module.scss";
+
+const cx = classNames.bind(styles);
+
 export default function Register() {
   const preparedEmail = useAccount((state) => state.registerEmail);
   const [registerForm, , setRegisterFormValue] =
@@ -22,42 +27,18 @@ export default function Register() {
   return (
     <>
       <DefaultHeader />
-      <main className={styles.Register}>
-        <h1 className={styles.title}>가입 완료까지 한 발자국! 👣</h1>
-        <div className={styles.description}>
-          본격적으로 이벤트를 구경하기 전,
-          <br /> 실명과 전화번호를 적어주세요.
-          <br /> 이벤트 예매나 예약 시 필요해요!
+      <main className={cx("Register")}>
+        <Progress steps={2} currentStep={2} />
+        <h1 className={cx("title")}>가입 완료까지 한 발자국! 👣</h1>
+        <div className={cx("description")}>
+          W@NDER 시작 전 이름과 전화번호를 작성해주세요.
+          <br />
+          해당 정보는 이벤트 참여와 예약 시 활용됩니다.
         </div>
-        <div className={styles.forms}>
-          <TextInput
-            title="이메일"
-            placeholder=""
-            value={registerForm.email}
-            onChange={(e) => setRegisterFormValue("email", e.target.value)}
-            isDisabled={true}
-          />
-          <TextInput
-            title="이름"
-            placeholder="ex. 장원영"
-            value={registerForm.name}
-            onChange={(e) => setRegisterFormValue("name", e.target.value)}
-            maxLength={30}
-          />
-          <TextInput
-            title="전화번호"
-            placeholder="ex. 010-1234-9678"
-            value={registerForm.phoneNumber}
-            onChange={(e) =>
-              setRegisterFormValue("phoneNumber", e.target.value)
-            }
-            maxLength={30}
-          />
-        </div>
-        <Button
-          label="완료!"
-          attribute={{ size: "big", theme: "default" }}
-          onClick={() => {
+        <form
+          className={cx("form")}
+          onSubmit={(e) => {
+            e.preventDefault();
             if (
               isValidRegisterEmail(registerForm.email) &&
               isValidRegisterName(registerForm.name) &&
@@ -68,7 +49,25 @@ export default function Register() {
               alert("회원가입 정보가 올바르지 않습니다.");
             }
           }}
-        />
+        >
+          <Input.Text
+            title="이름"
+            placeholder="ex. 홍길동"
+            value={registerForm.name}
+            onChange={(e) => setRegisterFormValue("name", e.target.value)}
+            maxLength={30}
+          />
+          <Input.Text
+            title="전화번호"
+            placeholder="ex. 010-1234-9678"
+            value={registerForm.phoneNumber}
+            onChange={(e) =>
+              setRegisterFormValue("phoneNumber", e.target.value)
+            }
+            maxLength={30}
+          />
+        </form>
+        <Button.Submit name="완료!" isFullWidth isMainColored />
       </main>
     </>
   );
