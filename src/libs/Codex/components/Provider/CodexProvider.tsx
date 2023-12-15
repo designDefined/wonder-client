1829841ca87e6acc3f92bd3ec4a54f35ea9e0f8b
designDefined <designdefined.by@gmail.com>
2023-12-15 15:33:55 +0900

    Fix: with css & js

--- src/libs/Codex/components/Provider/CodexProvider.tsx
+++ src/libs/Codex/components/Provider/CodexProvider.tsx
@@ -9,10 +9,14 @@ type Props = {
   provider: Codex;
 };
 
+let vh = 0;
+
 export default function CodexProvider({ provider }: Props) {
   const provide = useCodex((state) => state.provide);
 
   useEffect(() => {
+    vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
     provide(provider);
   }, [provider]);
 
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -14,14 +14,15 @@ const cx = classNames.bind(styles);
 
 export default function Login() {
   return (
-    <main className={cx("Login")}>
+    <>
       <LoginHeader />
-      <h2 className={cx("title")}>SNS 계정으로 간편 로그인</h2>
-      <div className={cx("buttons")}>
-        <button
-          className={cx("kakaoLogin")}
-          onClick={() => {
-            /*
+      <main className={cx("Login")}>
+        <h2 className={cx("title")}>SNS 계정으로 간편 로그인</h2>
+        <div className={cx("buttons")}>
+          <button
+            className={cx("kakaoLogin")}
+            onClick={() => {
+              /*
             api
               .post<UserSummary>("/user/login", { code: "test" })
               .then((res) => {
@@ -31,43 +32,44 @@ export default function Login() {
               })
               .catch(async (err) => console.log(await err));
                */
-            openTray(() => <SampleLoginTray />);
-          }}
-        >
-          <img src={kakao_icon} alt="kakao icon" />
-          <div>카카오 계정으로 로그인하기</div>
-        </button>
-        <button
-          className={cx("naverLogin")}
-          onClick={() => {
-            const url =
-              "https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=ea1D05CBMzfotDOECOFw&state=naver&redirect_uri=http://localhost:5173/naver";
-            location.href = url;
-          }}
-        >
-          <img src={naver_icon} alt="naver icon" />
-          <div>네이버 계정으로 로그인하기</div>
-        </button>
-        <button
-          className={cx("googleLogin")}
-          onClick={() => {
-            const url =
-              "https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=ea1D05CBMzfotDOECOFw&state=naver&redirect_uri=http://localhost:5173/naver";
-            location.href = url;
-          }}
-        >
-          <img src={google_icon} alt="google icon" />
-          <div> 구글 계정으로 로그인하기</div>
-        </button>
-        <button
-          className={cx("googleLogin")}
-          onClick={() => {
-            openTray(() => <Form />);
-          }}
-        >
-          <div>테스트 계정으로 로그인하기</div>
-        </button>
-      </div>
-    </main>
+              openTray(() => <SampleLoginTray />);
+            }}
+          >
+            <img src={kakao_icon} alt="kakao icon" />
+            <div>카카오 계정으로 로그인하기</div>
+          </button>
+          <button
+            className={cx("naverLogin")}
+            onClick={() => {
+              const url =
+                "https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=ea1D05CBMzfotDOECOFw&state=naver&redirect_uri=http://localhost:5173/naver";
+              location.href = url;
+            }}
+          >
+            <img src={naver_icon} alt="naver icon" />
+            <div>네이버 계정으로 로그인하기</div>
+          </button>
+          <button
+            className={cx("googleLogin")}
+            onClick={() => {
+              const url =
+                "https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=ea1D05CBMzfotDOECOFw&state=naver&redirect_uri=http://localhost:5173/naver";
+              location.href = url;
+            }}
+          >
+            <img src={google_icon} alt="google icon" />
+            <div> 구글 계정으로 로그인하기</div>
+          </button>
+          <button
+            className={cx("googleLogin")}
+            onClick={() => {
+              openTray(() => <Form />);
+            }}
+          >
+            <div>테스트 계정으로 로그인하기</div>
+          </button>
+        </div>
+      </main>
+    </>
   );
 }
